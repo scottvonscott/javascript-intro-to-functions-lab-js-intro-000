590f7d4ac6bfcacb71721dddaa1ba56a8c2a3a26
Scott Keplinger <skeplin17@gmail.com>
2020-01-10 04:39:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
+var uppercase = "HELLO"
+
+var lowercase = "hello"
+
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+   return "I love you, too."
+} else if (string === lowercase) {
+   return "I can't hear you!"
+} else {
+   return "YES INDEED!"
+ }
+ }
